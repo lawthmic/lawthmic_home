366d1a8e50e7be414819b57364c730daa9f1147e
Dr. Park's Macbook Air M1 <drpark@Drui-MacBookAir.local>
2021-04-16 09:57:10 +0900

    lawthmic start

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,7 +6,7 @@ import '../css/styles.css';
 function Intro() {
     return (
         <div class="IntroWrapper">
-            <p class="IntroText">In the era of 4th Industrial Revolution we provide unprecedented digital contents through the convergence of Law and Engineering.</p>
+            <p class="IntroText">We provide unprecedented digital contents through the Convergence of Law and Engineering.</p>
             <Image url="lawthmic_ci.png" caption="LAWTHMIC=LAW+algoriTHMIC" />
         </div>
     );
