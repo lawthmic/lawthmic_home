92b5b8e978e441f9c861f5d7937eade230232ad8
Dr. Park's Macbook Air M1 <drpark@Drui-MacBookAir.local>
2021-04-16 19:43:23 +0900

    visitor counter

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,6 +5,7 @@ function Title(props) {
         <div class="Header">
             <h2>{props.text}</h2>
         </div>
+        <a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Flawthmic.com&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false"/></a>
     );
 }
 
