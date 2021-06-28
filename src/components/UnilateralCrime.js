c2c11613503c5c8e17313529785fc362218431ec
Dr. Park's Macbook Air M1 <drpark@lawthmic-macBook-Air.local>
2021-06-28 19:14:31 +0900

    sg

--- src/components/UnilateralCrime.js
+++ src/components/UnilateralCrime.js
@@ -13,19 +13,19 @@ function UnilateralCrime() {
                 <th>2017년</th>
             </tr>
             <tr>
-                <td>전체범죄 건수</td>
+                <td>전체범죄</td>
                 <td>1,342,854</td>
                 <td>1,328,609</td>
                 <td>1,413,717</td>
             </tr>
             <tr>
-                <td>단독범죄 건수</td>
+                <td>단독범죄</td>
                 <td>1,220,325</td>
                 <td>1,206,049</td>
                 <td>1,284,499</td>
             </tr>
             <tr>
-                <td>단독범죄 비율(%)</td>
+                <td>비율(%)</td>
                 <td>90.8</td>
                 <td>90.8</td>
                 <td>90.9</td>
