813075120e1798ff75a13d826cbd5f19bba71efc
Dr. Park's Macbook Air M1 <drpark@lawthmic-macBook-Air.local>
2021-08-02 08:05:46 +0900

    jurisin

--- src/components/News.js
+++ src/components/News.js
@@ -3,13 +3,20 @@ import React from 'react';
 const lawthmic_news = [
     {
         id: 1,
-        name: '[구인]',
-        title: '책편집자(인디자인/재택근무)',
-        year: '2021. 7',
-        status: '모집중'
+        name: '[Call for Papers]',
+        title: 'FIFTEENTH INTERNATIONAL WORKSHOP ON JURIS-INFORMATICS(JURISIN)',
+        year: '2021',
+        status: 'Deadline: August 31'
     },
     {
         id: 2,
+        name: '[구인]',
+        title: '책편집자(인디자인/재택근무)',
+        year: '2021. 8',
+        status: '모집중'
+    },
+    {
+        id: 3,
         name: '[구독]',
         title: '프로그래밍 자료(raywenderlich.com)',
         year: '2021. 5',
