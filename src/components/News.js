3211e27c1ca93dd3cc0b2402e1358bc3f7b54bc3
Dr. Park's Macbook Air M1 <drpark@lawthmic-m1Air.local>
2021-11-12 19:38:04 +0900

    JURISIN

--- src/components/News.js
+++ src/components/News.js
@@ -2,15 +2,22 @@ import React from 'react';
 
 const lawthmic_news = [
     {
-        id: 1,
+        id: 6,
+        name: '[학회]',
+        title: '15th International Workshop on Juris-informatics(JURISIN 2021)',
+        year: 'November 14-15',
+        status: 'https://www.kl.itc.nagoya-u.ac.jp/jurisin2021/'
+    },
+    {
+        id: 5,
         name: '[정보]',
         title: 'International Association for AI and Law',
         year: '공식 유튜브 채널',
         status: 'https://www.youtube.com/channel/UC-nPeKR_Ey5EDrUAFr_4mEg'
     },
     {
-        id: 2,
-        name: '[정보]',
+        id: 4,
+        name: '[저널]',
         title: 'Journal of Artificial Intelligence and Law',
         year: 'Springer',
         status: 'SCIE·SSCI·SCOPUS'
@@ -23,14 +30,14 @@ const lawthmic_news = [
         status: 'https://law.stanford.edu/codex-the-stanford-center-for-legal-informatics/'
     },
     {
-        id: 4,
+        id: 2,
         name: '[구독]',
         title: '프로그래밍 학습(raywenderlich)',
         year: '2021. 5',
         status: '1년간'
     },
     {
-        id: 5,
+        id: 1,
         name: '[구독]',
         title: '영작문 도구(grammarly)',
         year: '2021. 9',
