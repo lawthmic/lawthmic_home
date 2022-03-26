1710f8bcc24444e2b618c1751be4fdb1394eec5a
Dr. Park's Macbook Air M1 <drpark@lawthmic-m1Air.local>
2022-03-26 20:45:03 +0900

    logo

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -7,7 +7,7 @@ function Intro() {
     return (
         <div class="IntroWrapper">
             <p class="IntroText">We provide unprecedented content through the convergence of Law and Science.</p>
-            <Image url="lawyer_badge-416x416.png" />
+            <Image url="cropped-lawyer_badge-300x300.png" />
         </div>
     );
 }
