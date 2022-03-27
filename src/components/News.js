82e7a66d0580aeccdd0ace38488baf9fc3fa6884
Dr. Park's Macbook Air M1 <drpark@lawthmic-m1Air.local>
2022-03-27 21:57:21 +0900

    news

--- src/components/News.js
+++ src/components/News.js
@@ -2,48 +2,12 @@ import React from 'react';
 
 const lawthmic_news = [
     {
-        id: 6,
-        name: '[학회]',
-        title: '15th International Workshop on Juris-informatics(JURISIN 2021)',
-        year: 'November 14-15',
-        status: ''
-    },
-    {
-        id: 5,
-        name: '[정보]',
-        title: 'International Association for AI and Law',
-        year: '공식 유튜브 채널',
-        status: ''
-    },
-    {
-        id: 4,
-        name: '[저널]',
-        title: 'Journal of Artificial Intelligence and Law',
-        year: 'Springer',
-        status: 'SCIE·SSCI·SCOPUS'
-    },
-    {
-        id: 3,
-        name: '[정보]',
-        title: 'CodeX (computational law)',
-        year: 'Stanford Univ.',
-        status: ''
-    },
-    {
-        id: 2,
-        name: '[구독]',
-        title: '프로그래밍 학습(raywenderlich)',
-        year: '2021. 5',
-        status: '1년간'
-    },
-    {
         id: 1,
-        name: '[구독]',
-        title: '영작문 도구(grammarly)',
-        year: '2021. 9',
-        status: '1년간'      
+        name: '[행사]',
+        title: '로드믹 사업계획 발표',
+        year: '2022년 4월 2일',
+        status: '본점 회의실'
     }
-    
 ]
 
 
@@ -52,7 +16,7 @@ function News() {
     return (
         
         <ul class="MethodWrapper">
-            {lawthmic_news.map(news => (
+            {lawthmic_news.slice(0).reverse().map(news => (
                 <li>{news.name} {news.title}, {news.year}, {news.status}</li>
             ))}
         </ul>
