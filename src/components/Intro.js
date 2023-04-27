9aa57c2619531e59d6b0d36aa30288c89f5de33d
Dr. Park's Macbook Air M1 <drpark@lawthmic-m1Air.local>
2023-04-27 13:09:15 +0900

    case

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,7 +6,7 @@ import '../css/styles.css';
 function Intro() {
     return (
         <div class="IntroWrapper">
-            <p class="IntroText">Lawthmic combines law and algorithm. We provide law-related content rooted in computer science, making it easily accessible and understandable for everyone.</p>
+            Lawthmic combines law and algorithm. We provide law-related content rooted in computer science, making it easily accessible and understandable for everyone.
             <Image url="lawyer-badge-800x533.png" />
         </div>
     );
