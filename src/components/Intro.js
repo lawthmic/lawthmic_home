f0b40eb909f2a13a2f964ee9c707a0c591c15e34
Dr. Park's Macbook Air M1 <drpark@lawthmic-m1Air.local>
2023-05-17 13:24:24 +0900

    case

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,7 +6,7 @@ import '../css/styles.css';
 function Intro() {
     return (
         <div class="IntroWrapper">
-            <p class="IntroText">We provide law-related content based on computer science, making it easily accessible and understandable for everyone.</p>
+            <p class="IntroText">We provide legal content based on computer science, making it easily accessible and understandable for everyone.</p>
             <Image url="lawyer-badge-800x533.png" />
         </div>
     );
